4ba9329498ab8d41f3d6c354f252f3ca70c95c80
dhiatnit <dhiatunsien@gmail.com>
2026-02-25 01:04:37 +0100

    Dhia (#1)
    * created DB and linked it to the project
    
    * created DB and linked it to the project

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core'
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,13 +74,19 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.pexkauwlmdudtyztfabu",
+        "PASSWORD": "cattolica123",
+        "HOST": "aws-1-eu-west-2.pooler.supabase.com",
+        "PORT": "5432",
+        "CONN_MAX_AGE": 60,
+        "OPTIONS": {"sslmode": "require"},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
