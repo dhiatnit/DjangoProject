5150760484c029e77a5d086d1edce05fd7e802af
dhiatnit <dhiatunsien@gmail.com>
2026-02-25 02:39:25 +0100

    Dhia (#2)
    * created DB and linked it to the project
    
    * Enable SQL logging

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -88,6 +88,21 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
